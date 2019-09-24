83d9a6c08afc9af031eca946b5beca79701d6643
Joel Vaz <joelmatosvaz@gmail.com>
2019-09-24 15:52:03 +0100

    Created action for button decide/addFood and input text field.

--- app/src/main/java/com/example/droid_data/MainActivity.kt
+++ app/src/main/java/com/example/droid_data/MainActivity.kt
@@ -2,13 +2,27 @@ package com.example.droid_data
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val foodList= arrayListOf("Chinese", "Hamburguer", "Pizza", "Mc", "Italian")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        decideBtn.setOnClickListener {
+            val random= Random()
+            val randomFood= random.nextInt(foodList.count())
+            selectedfoodtext.text= foodList[randomFood]
+        }
 
+        addFoodBtn.setOnClickListener {
+            val newFood= addFoodText.text.toString()
+            foodList.add(newFood)
+            addFoodText.text.clear()
+        }
     }
 }
