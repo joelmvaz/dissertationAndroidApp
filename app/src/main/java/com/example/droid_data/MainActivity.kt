35ce710d893a47225786127e9e900a35589c036e
Joel Vaz <joelmatosvaz@gmail.com>
2019-09-25 15:59:37 +0100

    Implemented sensor reading, magnetism, gyroscope and gravity. && Created view for landscape mode.

--- app/src/main/java/com/example/droid_data/MainActivity.kt
+++ app/src/main/java/com/example/droid_data/MainActivity.kt
@@ -1,36 +1,66 @@
 package com.example.droid_data
 
 import android.content.Context
+import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
-    lateinit var sensorManager: SensorManager
+    private lateinit var sensorManager: SensorManager
     private var uniqueUserId= 1
     private var acceleration= ""
+    private var rotation= ""
+    private var gravity= ""
+    private var magnet= ""
+
+
 
     override fun onSensorChanged(event: SensorEvent?) {
-        acceleration= "   x= ${event?.values?.get(0)} m/s2\n" +
-                "   y= ${event?.values?.get(1)} m/s2\n" +
-                "   z= ${event?.values?.get(2)} m/s2"
 
-        accDta.text= acceleration
+        when (event?.sensor?.type) {
+
+            Sensor.TYPE_ACCELEROMETER -> {
+                acceleration = "x = ${event.values[0]} m/s2\n" +
+                        "y = ${event.values[1]} m/s2\n" +
+                        "z = ${event.values[2]} m/s2"
+                accDta.text = acceleration
+            }
+
+            Sensor.TYPE_GYROSCOPE -> {
+                rotation = "x = ${event.values[0]} rad/s\n" +
+                        "y = ${event.values[1]} rad/s \n" +
+                        "z = ${event.values[2]} rad/s"
+                rotDta.text = rotation
+            }
+            Sensor.TYPE_GRAVITY -> {
+                gravity = "y = ${event.values[1]} m/s2\n"
+                gravDta.text = gravity
+            }
+            Sensor.TYPE_MAGNETIC_FIELD -> {
+                magnet = "x = ${event.values[0]} T\n" +
+                        "y = ${event.values[1]} T \n" +
+                        "z = ${event.values[2]} T"
+                magDta.text = magnet
+            }
+        }
     }
 
-
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Sets the user id
+        usrId.text= uniqueUserId.toString()
 
         sensorManager= getSystemService(Context.SENSOR_SERVICE) as SensorManager
         sensorManager.registerListener(
@@ -38,8 +68,20 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
             SensorManager.SENSOR_DELAY_NORMAL
         )
-
-        //Sets the user id
-        usrId.text= uniqueUserId.toString()
+        sensorManager.registerListener(
+            this,
+            sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE),
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
+        sensorManager.registerListener(
+            this,
+            sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY),
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
+        sensorManager.registerListener(
+            this,
+            sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
     }
 }
