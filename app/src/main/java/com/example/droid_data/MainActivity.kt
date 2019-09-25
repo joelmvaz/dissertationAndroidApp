1a07d270f8290e9d38959b4e1bf7b020d748c2c5
Joel Vaz <joelmatosvaz@gmail.com>
2019-09-25 11:40:50 +0100

    Implemented acceleration reading data. && Removed legacy code from application prototype. && Improved UX.

--- app/src/main/java/com/example/droid_data/MainActivity.kt
+++ app/src/main/java/com/example/droid_data/MainActivity.kt
@@ -1,30 +1,45 @@
 package com.example.droid_data
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
 
-    private val foodList= arrayListOf("Chinese", "Hamburguer", "Pizza", "Mc", "Italian")
+    lateinit var sensorManager: SensorManager
+    private var uniqueUserId= 1
+    private var acceleration= ""
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        acceleration= "   x= ${event?.values?.get(0)} m/s2\n" +
+                "   y= ${event?.values?.get(1)} m/s2\n" +
+                "   z= ${event?.values?.get(2)} m/s2"
+
+        accDta.text= acceleration
+    }
+
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        decideBtn.setOnClickListener {
-            val random= Random()
-            val randomFood= random.nextInt(foodList.count())
-            selectedfoodtext.text= foodList[randomFood]
-        }
+        sensorManager= getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensorManager.registerListener(
+            this,
+            sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
 
-        addFoodBtn.setOnClickListener {
-            val newFood= addFoodText.text.toString()
-            if (!newFood.isBlank()) {
-                foodList.add(newFood)
-                addFoodText.text.clear()
-            }
-        }
+        //Sets the user id
+        usrId.text= uniqueUserId.toString()
     }
 }
