69d7ba7fdc71d5a19aaa70e4c2d41635fbf32372
Joel Vaz <joelmatosvaz@gmail.com>
2019-09-27 15:28:39 +0100

    Implemented location service tracking.

--- app/src/main/java/com/example/droid_data/MainActivity.kt
+++ app/src/main/java/com/example/droid_data/MainActivity.kt
@@ -1,26 +1,33 @@
 package com.example.droid_data
 
+import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.view.View
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private lateinit var sensorManager: SensorManager
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    lateinit var locationRequest: LocationRequest
+    lateinit var locationCallback: LocationCallback
+
+    private val requestCode= 1000
     private var uniqueUserId= 1
     private var acceleration= ""
     private var rotation= ""
     private var gravity= ""
     private var magnet= ""
-
-
 
     override fun onSensorChanged(event: SensorEvent?) {
 
@@ -40,7 +47,9 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
                 rotDta.text = rotation
             }
             Sensor.TYPE_GRAVITY -> {
-                gravity = "y = ${event.values[1]} m/s2\n"
+                gravity = "x = ${event.values[0]} m/s\n" +
+                        "y = ${event.values[1]} m/s \n" +
+                        "z = ${event.values[2]} m/s"
                 gravDta.text = gravity
             }
             Sensor.TYPE_MAGNETIC_FIELD -> {
@@ -54,6 +63,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
     }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -83,5 +93,57 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD),
             SensorManager.SENSOR_DELAY_NORMAL
         )
+
+        /* Check Permissions For Location Services*/
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)){
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), requestCode)
+        }else{
+            buildLocationRequest()
+            buildLocationCallBack()
+
+            /* Create FusedProviderClient */
+            fusedLocationProviderClient= LocationServices.getFusedLocationProviderClient(this)
+            /* Set Location*/
+            /* ADD BTN */
+            btnUpdates.setOnClickListener(View.OnClickListener {
+                if (ActivityCompat.checkSelfPermission(this@MainActivity,
+                        Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(this@MainActivity,
+                        Manifest.permission.ACCESS_COARSE_LOCATION) !=
+                    PackageManager.PERMISSION_GRANTED) {
+
+                    ActivityCompat.requestPermissions(this@MainActivity,
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), requestCode)
+                    return@OnClickListener
+                }
+                fusedLocationProviderClient.requestLocationUpdates(
+                    locationRequest,
+                    locationCallback, Looper.myLooper())
+                /* Change State of button*/
+                btnUpdates.isEnabled= !btnUpdates.isEnabled
+            })
+        }
+    }
+
+    private fun buildLocationCallBack() {
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(p0: LocationResult?) {
+                /* Get Last Location*/
+                val location = p0!!.locations.get(p0!!.locations.size - 1)
+                gpsDta.text = "Latitude: " + location.latitude + "º\n" +
+                        "Longitude: " + location.longitude + "º\n" + "Speed: " +
+                        location.speed + "m/s\n"
+            }
+        }
+    }
+
+    private fun buildLocationRequest() {
+        locationRequest= LocationRequest()
+        locationRequest.priority= LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval= 5000
+        locationRequest.fastestInterval= 3000
+        locationRequest.smallestDisplacement= 10f
     }
 }
